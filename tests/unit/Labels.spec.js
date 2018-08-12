c03b634a24e2bb447666f3ce3b3e799a0f3fcd65
Jeff <jeff.sagal@briteweb.com>
2018-08-12 13:42:07 -0700

    Complete labels spec

--- tests/unit/Labels.spec.js
+++ tests/unit/Labels.spec.js
@@ -1,48 +1,42 @@
-import { shallowMount } from "@vue/test-utils";
 import VueSelect from "../../src/components/Select";
+import { shallowMount } from "@vue/test-utils";
+import { selectWithProps } from "../helpers";
 
 describe("Labels", () => {
   it("can generate labels using a custom label key", () => {
-    const Select = shallowMount(VueSelect, {
-      propsData: {
-        options: [{ name: "Foo" }],
-        label: "name",
-        value: { name: "Foo" }
-      }
+    const Select = selectWithProps({
+      options: [{ name: "Foo" }],
+      label: "name",
+      value: { name: "Foo" }
     });
     expect(Select.find(".selected-tag").text()).toBe("Foo");
   });
-  //
-  //   it("will console.warn when options contain objects without a valid label key", done => {
-  //     spyOn(console, "warn");
-  //     const vm = new Vue({
-  //       template: '<div><v-select :options="[{}]"></v-select></div>'
-  //     }).$mount();
-  //     vm.$children[0].open = true;
-  //     Vue.nextTick(() => {
-  //       expect(console.warn).toHaveBeenCalledWith(
-  //         '[vue-select warn]: Label key "option.label" does not exist in options object {}.' +
-  //         "\nhttp://sagalbot.github.io/vue-select/#ex-labels"
-  //       );
-  //       done();
-  //     });
-  //   });
-  //
-  //   it("should display a placeholder if the value is empty", done => {
-  //     const vm = new Vue({
-  //       template:
-  //         '<div><v-select :options="options" placeholder="foo"></v-select></div>',
-  //       components: { vSelect },
-  //       data: {
-  //         options: [{ label: "one" }]
-  //       }
-  //     }).$mount();
-  //
-  //     expect(vm.$children[0].searchPlaceholder).toEqual("foo");
-  //     vm.$children[0].mutableValue = { label: "one" };
-  //     Vue.nextTick(() => {
-  //       expect(vm.$children[0].searchPlaceholder).not.toBeDefined();
-  //       done();
-  //     });
-  //   });
+
+  it("will console.warn when options contain objects without a valid label key", () => {
+    const spy = jest.spyOn(console, "warn");
+    const Select = selectWithProps({
+      options: [{}]
+    });
+
+    Select.vm.open = true;
+    expect(spy).toHaveBeenCalledWith(
+      '[vue-select warn]: Label key "option.label" does not exist in options object {}.' +
+        "\nhttp://sagalbot.github.io/vue-select/#ex-labels"
+    );
+  });
+
+  it("should display a placeholder if the value is empty", () => {
+    const Select = shallowMount(VueSelect, {
+      propsData: {
+        options: [{ label: "one" }]
+      },
+      attrs: {
+        placeholder: "foo"
+      }
+    });
+
+    expect(Select.vm.searchPlaceholder).toEqual("foo");
+    Select.vm.mutableValue = { label: "one" };
+    expect(Select.vm.searchPlaceholder).not.toBeDefined();
+  });
 });

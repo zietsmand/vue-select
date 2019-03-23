5ae658c84cdfbcc60efa622ac69af2d6498ee50e
Jeff <jeff.sagal@briteweb.com>
2019-03-23 14:13:31 -0700

    update package deps, remove extra dev options

--- dev/Dev.vue
+++ dev/Dev.vue
@@ -2,20 +2,7 @@
   <div id="app">
     <sandbox hide-help>
       <template slot-scope="config">
-      
-      <p>Value managed by `v-model`:</p>
-      <v-select v-model="vModelValue" v-bind="config" />
-      <pre><code>`v-model` value: {{ vModelValueStringified }}</code></pre>
-      <hr />
-
-      <p>Value managed by `:value` and `@input`:</p>
-      <v-select :value="valueProp" @input="changeValueProp" v-bind="config" />
-      <pre><code>value passed to `@input`: {{ valuePropStringified }}</code></pre>
-      <hr />
-
-      <p>Value managed by Vue Select internally:</p>
-      <v-select v-bind="config" />
-
+        <v-select v-model="value" v-bind="config" />
       </template>
     </sandbox>
   </div>
@@ -29,30 +16,6 @@ import Sandbox from '../docs/.vuepress/components/Sandbox';
 
 export default {
   components: {Sandbox, vSelect},
-  data: () => ({
-    vModelValue: {
-      value: 'CA',
-      label: 'Canada'
-    },
-    valueProp: {
-      value: 'US',
-      label: 'United States'
-    }
-  }),
-  methods: {
-    changeValueProp(value) {
-      this.valueProp = value;
-    }
-  },
-  computed: {
-    vModelValueStringified() {
-      return JSON.stringify(this.vModelValue, null, 2);
-    },
-
-    valuePropStringified() {
-      return JSON.stringify(this.valueProp, null, 2);
-    }
-  }
 };
 </script>
 

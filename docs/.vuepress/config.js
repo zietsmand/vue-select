d522acacfd9086e08e93404309a10b47fba4b5b2
Jeff <jeff.sagal@briteweb.com>
2019-04-18 16:13:43 -0500

    bump meta description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const isDeployPreview = process.env.hasOwnProperty('DEPLOY_PREVIEW');
 
 const meta = {
   title: 'Vue Select | VueJS Select2/Chosen Component',
-  description: 'Everything you wish the HTML &#x3C;select&#x3E; element could do, wrapped up into a lightweight, extensible Vue component.',
+  description: 'Everything you wish the HTML select element could do, wrapped up into a lightweight, extensible Vue component.',
   url: 'https://vue-select.org',
 };
 

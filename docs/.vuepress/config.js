83e59241fe9973d83c3cf768e8855ac5984a80fb
Jeff Sagal <sagalbot@gmail.com>
2019-04-18 11:49:01 -0500

    Vuepress 1.0 (#843)
    * bump vuepress
    
    * extend default-theme, implement codefund

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const isDeployPreview = process.env.hasOwnProperty('DEPLOY_PREVIEW');
 
 const meta = {
   title: 'Vue Select | VueJS Select2/Chosen Component',
-  description: 'Everything you wish the HTML <select> element could do, wrapped up into a lightweight, extensible Vue component.',
+  description: 'Everything you wish the HTML &#x3C;select&#x3E; element could do, wrapped up into a lightweight, extensible Vue component.',
   url: 'http://sagalbot.github.io/vue-select/',
   icon: '/vue-logo.png',
 };
@@ -22,14 +22,35 @@ let head = [
       rel: 'stylesheet',
       type: 'text/css',
     }],
-  ['link', { rel: 'icon', href: `/vue-logo.png` }],
-  ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-  ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-  ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-  ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-  ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-  ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ['link', {rel: 'icon', href: `/vue-logo.png`}],
+  ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+  ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+  ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+  [
+    'link',
+    {rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png`}],
+  [
+    'link',
+    {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
+  [
+    'meta',
+    {
+      name: 'msapplication-TileImage',
+      content: '/icons/msapplication-icon-144x144.png',
+    }],
+  ['meta', {name: 'msapplication-TileColor', content: '#000000'}],
+  ['meta', {name: 'title', content: meta.title}],
+  ['meta', {name: 'description', content: meta.description}],
+  ['link', {rel: 'icon', href: meta.icon, type: 'image/png'}],
+  ['meta', {property: 'og:image', content: meta.icon}],
+  ['meta', {property: 'twitter:image', content: meta.icon}],
+  ['meta', {name: 'description', content: meta.description}],
+  ['meta', {property: 'og:description', content: ''}],
+  ['meta', {property: 'twitter:description', content: meta.description}],
+  ['meta', {property: 'twitter:title', content: meta.title}],
+  ['meta', {property: 'og:title', content: meta.title}],
+  ['meta', {property: 'og:site_name', content: meta.title}],
+  ['meta', {property: 'og:url', content: meta.url}],
 ];
 
 if (isDeployPreview) {
@@ -37,29 +58,24 @@ if (isDeployPreview) {
     ['meta', {name: 'robots', content: 'noindex'}],
     ['meta', {name: 'googlebot', content: 'noindex'}],
   );
-} else {
-  head.push(
-    ['meta', {name: 'title', content: meta.title}],
-    ['meta', {name: 'description', content: meta.description}],
-    ['link', {rel: 'icon', href: meta.icon, type: 'image/png'}],
-    ['meta', {property: 'og:image', content: meta.icon}],
-    ['meta', {property: 'twitter:image', content: meta.icon}],
-    ['meta', {name: 'description', content: meta.description}],
-    ['meta', {property: 'og:description', content: ''}],
-    ['meta', {property: 'twitter:description', content: meta.description}],
-    ['meta', {property: 'twitter:title', content: meta.title}],
-    ['meta', {property: 'og:title', content: meta.title}],
-    ['meta', {property: 'og:site_name', content: meta.title}],
-    ['meta', {property: 'og:url', content: meta.url}],
-  );
 }
 
 module.exports = {
   title: 'Vue Select',
   description: meta.description,
   head,
-  serviceWorker: !isDeployPreview,
-  ga: isDeployPreview ? '' : 'UA-12818324-8',
+  plugins: {
+    '@vuepress/google-analytics': {
+      ga: isDeployPreview ? '' : 'UA-12818324-8',
+    },
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: {
+        message: 'New content is available.',
+        buttonText: 'Refresh',
+      },
+    },
+  },
   themeConfig: {
     repo: 'sagalbot/vue-select',
     editLinks: true,

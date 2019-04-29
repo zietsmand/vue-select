074c0b66861363083767cf56ecbd69680a945cc8
Jeff <jeff.sagal@briteweb.com>
2019-04-28 22:25:22 -0700

    Add reproduction for #790

---
+++ docs/.vuepress/components/BooksWithSlots.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-select :options="books" label="title">
+    <template #selected-option="{author, title}">
+      <component :is="option" v-bind="{author, title}"/>
+    </template>
+    <template #option="{author, title}">
+      <component :is="option" v-bind="{author, title}"/>
+    </template>
+  </v-select>
+</template>
+
+<script>
+import vSelect from '../../../src/components/Select';
+import books from '../data/books';
+
+export default {
+  components: {vSelect},
+  computed: {
+    books: () => books,
+    option: () => ({
+      functional: true,
+      render: (h, {props}) => h('div', {class: {author: true}}, [
+        h('strong', props.title),
+        h('i', `${props.author.firstName} ${props.author.lastName}`),
+      ]),
+    }),
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

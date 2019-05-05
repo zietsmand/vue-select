25be6bd8db8961c07f2699bbdac134a42c02ab3b
Jeff <jeff.sagal@briteweb.com>
2019-05-05 15:07:50 -0700

    add type declaration

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for vue-select
+// Project: vue-select
+// Definitions by: Jeff Sagal <https://twitter.com/sagalbot>
+
+import Vue from 'vue';
+
+declare class Select extends Vue {}
+
+export default Select
